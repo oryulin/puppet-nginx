5daad68458bfd0d4ac1150e96bd61991f610bbf8
root <root@puppetmaster.lan>
2017-07-07 19:51:13 +0000

    adding epel-release package to be installed before nginx

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,11 @@
 class nginx::install inherits nginx {
   
+  # Installing pre-req package:
+  package { 'install_epel':
+    ensure => $ensure,
+    name   => 'epel-release',
+  }
+  
   package { 'install_nginx':
     ensure => $ensure,
     name   => $package_name,
