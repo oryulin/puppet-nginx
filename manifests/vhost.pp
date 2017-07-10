a61bb6d32a94dc0ebcce035268cf68335b435e50
oryulin@gmail.com <oryulin@gmail.com>
2017-07-10 17:23:27 +0000

    update

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -3,11 +3,11 @@ define nginx::vhost(
   String $priority = '50',
   String $server_aliases = '',
   Boolean $enable = true,
-  $docroot = lookup('nginx::docroot'),
-  $vhost_dir = lookup('nginx::vhost_dir'),
-  $config_mode = lookup('nginx::config_mode'),
-  $config_owner = lookup('nginx::config_owner'),
-  $config_group = lookup('nginx::config_group'),
+  $docroot = lookup('::nginx::docroot'),
+  $vhost_dir = lookup('::nginx::vhost_dir'),
+  $config_mode = lookup('::nginx::config_mode'),
+  $config_owner = lookup('::nginx::config_owner'),
+  $config_group = lookup('::nginx::config_group'),
 ) {
   
 #  include nginx::data
