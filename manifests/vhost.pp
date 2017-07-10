f875d7715eefcb22342590b71d75b65a103944be
oryulin@gmail.com <oryulin@gmail.com>
2017-07-10 16:42:59 +0000

    added notify in vhost

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -4,7 +4,9 @@ define nginx::vhost(
   String $server_aliases = '',
   Boolean $enable = true,
 ) {
-   
+  
+  notify { "Creating the virtual host with data: \n $vhost_docroot - ${vhost_docroot} \n $vhost_dir - ${vhost_dir} \n $priority - ${priority} \n $name - ${name}": }
+     
   include nginx
   
   $vhost_docroot = "${docroot}/${name}"
