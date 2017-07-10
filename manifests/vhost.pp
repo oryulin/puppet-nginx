3cb921319b0ab9a1eb741f4a304f026be3c9b90a
oryulin@gmail.com <oryulin@gmail.com>
2017-07-10 18:10:40 +0000

    update

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -13,7 +13,7 @@ define nginx::vhost(
 
 #  include nginx
   
-  notify { "Creating the virtual host with data: \n vhost_docroot - ${vhost_docroot} \n vhost_dir - ${vhost_dir} \n priority - ${priority} \n name - ${name} \n mode/owner/group - ${config_mode}/${config_owner}/${config_group}": }
+  notify { "Creating the virtual host with data: \n docroot - ${docroot} \n vhost_dir - ${vhost_dir} \n priority - ${priority} \n name - ${name} \n mode/owner/group - ${config_mode}/${config_owner}/${config_group}": }
   
   $vhost_docroot = "${docroot}/${name}"
   
